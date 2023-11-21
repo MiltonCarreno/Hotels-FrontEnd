24915463adee6a18247cc5018b8359e480f7e15a
MiltonCarreno <miltonyaredcr@gmail.com>
2023-11-20 19:38:40 -0800

    Added prototype of form to 'hotel' route to create a new review

--- src/routes/hotel/[hotel_id]/+page.server.js
+++ src/routes/hotel/[hotel_id]/+page.server.js
@@ -1,11 +1,36 @@
+import {redirect} from '@sveltejs/kit';
+
 export async function load({ fetch, params }) {
     const hotel_url = "http://0.0.0.0:8080/get_hotel/" 
         + params.hotel_id;
     const hotel_reviews_url = "http://0.0.0.0:8080/get_hotel_reviews/" 
         + params.hotel_id;
+    const users_url = "http://0.0.0.0:8080/get_all_users";
+
     const hotel = await fetch(hotel_url).then((data) => data.json());
     const reviews = await fetch(hotel_reviews_url).then((data) => data.json());
+    const users = await fetch(users_url).then((data) => data.json());
     console.log(hotel);
     console.log(reviews);
-    return {hotel: hotel, reviews: reviews};
+    console.log(users);
+    return {hotel: hotel, reviews: reviews, users: users};
+}
+
+export const actions = {
+    add: async ({ request }) => {
+        const data = await request.formData();
+        const username = data.get('username');
+        const email = data.get('email');
+        const hotel_id = data.get('hotel_id');
+        const url = "http://0.0.0.0:8080/add_user";
+
+        const res = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({ 'username': username, 'email': email}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        throw redirect(303, "http://localhost:5173/hotel/" + hotel_id);
+    }
 }
--- src/routes/hotel/[hotel_id]/+page.svelte
+++ src/routes/hotel/[hotel_id]/+page.svelte
@@ -2,6 +2,7 @@
     export let data;
     let hotel = data.hotel;
     let reviews = data.reviews;
+    let users = data.users;
 </script>
 
 <div>
@@ -9,6 +10,19 @@
     <p> Name: {hotel.name} </p>
     <p> Address: {hotel.address}, {hotel.city}, {hotel.country} </p>
 </div>
+
+<form method="POST" action="?/add">
+    Username: <input type="text" autocomplete="off" name="username">
+    Email: <input type="text" autocomplete="off" name="email">
+    <input type="hidden" name="hotel_id" value="{hotel.hotel_id}"/>
+    <input type="submit"/>
+</form>
+
+<ul>
+    {#each users as {username, email}}
+        <li>Name: {username} == Email: {email}</li>
+    {/each}
+</ul>
 
 <ul>
     {#each reviews as {author, title, rating}}
